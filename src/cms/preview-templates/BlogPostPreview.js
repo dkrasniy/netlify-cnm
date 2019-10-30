25d72313cae397975d072beb57a41d36f5ec77b4
David Krasniy <davidkrasniy@gmail.com>
2019-10-29 20:34:35 -0700

    image preview

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,14 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { BlogPostTemplate } from '../../templates/blog-post'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
-)
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  return  (
+    <BlogPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={entry.getIn(['data', 'tags'])}
+      title={entry.getIn(['data', 'title'])}
+      image={entry.getIn(['data', 'featuredimage'])}
+    />
+  )
+  
+}
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
